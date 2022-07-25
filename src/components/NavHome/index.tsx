f2f794a510cf7704f50caab05f78cb8372bb4f33
Hernande Monteiro <48896051+hernandemonteiro@users.noreply.github.com>
2022-07-24 21:52:23 -0300

    Code review NavHome

--- src/components/NavHome/index.tsx
+++ src/components/NavHome/index.tsx
@@ -2,27 +2,6 @@ import Link from 'next/link';
 import React from 'react';
 import Nav from '../NavTemplate';
 import ButtonNav from '../Buttons';
-import { request } from "../../lib/datocms";
-
-
-const NavHome_QUERY = `query NavHome($limit: IntType) {
-    allPosts(first: $limit) {
-      title,
-      shortdescription,
-      date,
-      category,
-      post
-      }
-  }`;
-export async function getStaticProps() {
-    const data = await request({
-        query: NavHome_QUERY,
-        variables: { limit: 10 }
-    });
-    return {
-        props: { data }
-    };
-}
 
 export default function NavHome({data}) {
     return (
