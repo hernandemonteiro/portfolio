bf3d1f1fbeecabc0d0619106895e00b5a4798801
Hernande Monteiro <48896051+hernandemonteiro@users.noreply.github.com>
2022-07-25 17:00:50 -0300

    style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,7 @@ import styles from "./Header.module.css";
 export default function Header() {
     return (
         <div className={styles.Header}>
-            <Typography className='m-2 Title' variant="h4">Hernande Monteiro</Typography>
+            <Typography className={styles.Title} variant="h4">Hernande Monteiro</Typography>
             <hr />
         </div>
     )
