205e355879f03e7e29daa133cc1ad9cf64952854
Hernande Monteiro <48896051+hernandemonteiro@users.noreply.github.com>
2022-07-30 15:25:44 -0300

    Preview Post

---
+++ src/pages/preview/index.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { useQuerySubscription } from "react-datocms";
+import { request } from "../../lib/datocms";
+import parse from 'html-react-parser';
+import Template from "../../components/Template";
+import Nav from "../../components/Nav";
+
+let query = `allPreviews(first: $limit) {
+    post
+    }`;
+  
+    const HOMEPAGE_QUERY = `query HomePage($limit: IntType) {
+      ${query}
+    }`;
+  
+    export async function getStaticProps() {
+  
+      const graphqlRequest = {
+        query: HOMEPAGE_QUERY,
+        variables: { limit: 1 },
+      };
+      
+      return {
+        props: {
+          subscription: {
+            ...graphqlRequest,
+            initialData: await request(graphqlRequest),
+            token: "59e2d095f8563442f2bb23b25ab172",
+          },
+        },
+      };
+    }
+
+export default function Artigo({subscription}) {
+    
+    const { data } = useQuerySubscription(subscription);
+
+    return (
+
+        <Template nav={<Nav data={data} />} >
+            {data.allPreviews[0].map((element) => {
+
+                    return parse(element.post)
+          
+            })
+
+            }
+        </Template>
+    )
+}
