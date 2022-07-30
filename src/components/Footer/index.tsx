e5e6901bf22b14d658cf38ad6355934ae31a1f46
Hernande Monteiro <48896051+hernandemonteiro@users.noreply.github.com>
2022-07-30 14:40:31 -0300

    icon style

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -17,19 +17,19 @@ export default function Footer() {
             >
                 <div className={styles.SocialIcons}>
                     <a href="/curriculum">
-                        <FontAwesomeIcon icon={faUserLarge} /> : Sobre Mim
+                        <FontAwesomeIcon className={styles.Icon} icon={faUserLarge} /> : Sobre Mim
                     </a>
                     <a href="/portfolio">
-                        <FontAwesomeIcon icon={faNewspaper} /> : Portfolio
+                        <FontAwesomeIcon className={styles.Icon} icon={faNewspaper} /> : Portfolio
                     </a>
                     <a href="https://github.com/hernandemonteiro">
-                        <FontAwesomeIcon icon={faGithub} /> : Github
+                        <FontAwesomeIcon className={styles.Icon} icon={faGithub} /> : Github
                     </a>
                     <a href="https://www.instagram.com/monteiro.ops/">
                         <FontAwesomeIcon icon={faInstagram} /> : Instagram
                     </a>
                     <a href="https://www.instagram.com/monteiro.ops/">
-                        <FontAwesomeIcon icon={faLinkedin} /> : Linkedin
+                        <FontAwesomeIcon className={styles.Icon} icon={faLinkedin} /> : Linkedin
                     </a>
                 </div>
                 <br />
