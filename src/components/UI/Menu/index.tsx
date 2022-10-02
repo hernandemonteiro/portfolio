b554f73f441e2555e85f8ba37716f5b5f916857b
Hernande Monteiro <monteiro1998@live.com>
2022-10-02 17:20:07 -0300

    color white in production fix error

--- src/components/UI/Menu/index.tsx
+++ src/components/UI/Menu/index.tsx
@@ -9,38 +9,61 @@ import Link from "next/link";
 import React from "react";
 import styles from "./Menu.module.css";
 
-
 export default function Menu() {
   return (
     <div className={styles.Menu}>
       <div className={styles.SocialIcon}>
         <Link href="/">
-          <FontAwesomeIcon color="white" icon={faBlog} fixedWidth />
+          <FontAwesomeIcon
+            style={{ color: "white" }}
+            icon={faBlog}
+            fixedWidth
+          />
         </Link>
       </div>
       <div className={styles.SocialIcon}>
         <Link href="/curriculum">
-          <FontAwesomeIcon color="white" icon={faUser} fixedWidth />
+          <FontAwesomeIcon
+            style={{ color: "white" }}
+            icon={faUser}
+            fixedWidth
+          />
         </Link>
       </div>
       <div className={styles.SocialIcon}>
         <Link href="/portfolio">
-          <FontAwesomeIcon color="white" icon={faNewspaper} fixedWidth />
+          <FontAwesomeIcon
+            style={{ color: "white" }}
+            icon={faNewspaper}
+            fixedWidth
+          />
         </Link>
       </div>
       <div className={styles.SocialIcon}>
         <Link href="https://github.com/hernandemonteiro" target="_blank">
-          <FontAwesomeIcon color="white" icon={faGithub} fixedWidth />
+          <FontAwesomeIcon
+            style={{ color: "white" }}
+            icon={faGithub}
+            fixedWidth
+          />
         </Link>
       </div>
       <div className={styles.SocialIcon}>
         <Link href="https://www.instagram.com/monteiro.ops/">
-          <FontAwesomeIcon color="white" icon={faInstagram} fixedWidth />
+          <FontAwesomeIcon
+            style={{ color: "white" }}
+            icon={faInstagram}
+            fixedWidth
+          />
         </Link>
       </div>
       <div className={styles.SocialIcon}>
         <Link href="https://br.linkedin.com/in/hernande-monteiro">
-          <FontAwesomeIcon color="white" icon={faLinkedin} fixedWidth />
+          <FontAwesomeIcon
+            style={{ color: "white" }}
+            icon={faLinkedin}
+            fixedWidth
+          />
         </Link>
       </div>
     </div>
