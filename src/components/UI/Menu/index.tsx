bfd8773d12468f905f732789b25c790679f5dbc4
Hernande Monteiro <monteiro1998@live.com>
2022-10-02 17:58:37 -0300

    link style

--- src/components/UI/Menu/index.tsx
+++ src/components/UI/Menu/index.tsx
@@ -12,94 +12,64 @@ import styles from "./Menu.module.css";
 export default function Menu() {
   return (
     <div className={styles.Menu} style={{ color: "white" }}>
-      <div className={styles.SocialIcon}>
-<<<<<<< Updated upstream
-        <a href="/" style={{ color: "white" }}>
-=======
-        <Link href="/" style={{ color: "white" }}>
->>>>>>> Stashed changes
-          <FontAwesomeIcon
-            style={{ color: "white" }}
-            icon={faBlog}
-            fixedWidth
-          />
-        </a>
-      </div>
-      <div className={styles.SocialIcon}>
-<<<<<<< Updated upstream
-        <a href="/curriculum" style={{ color: "white" }}>
-=======
-        <Link href="/curriculum" style={{ color: "white" }}>
->>>>>>> Stashed changes
-          <FontAwesomeIcon
-            style={{ color: "white" }}
-            icon={faUser}
-            fixedWidth
-          />
-        </a>
-      </div>
-      <div className={styles.SocialIcon}>
-<<<<<<< Updated upstream
-        <a href="/portfolio" style={{ color: "white" }}>
-=======
-        <Link href="/portfolio" style={{ color: "white" }}>
->>>>>>> Stashed changes
-          <FontAwesomeIcon
-            style={{ color: "white" }}
-            icon={faNewspaper}
-            fixedWidth
-          />
-        </a>
-      </div>
-      <div className={styles.SocialIcon} style={{ color: "white" }}>
-<<<<<<< Updated upstream
-        <a
-=======
-        <Link
->>>>>>> Stashed changes
-          href="https://github.com/hernandemonteiro"
+      <Link href="/" style={{ color: "white" }}>
+        <FontAwesomeIcon
+          className={styles.SocialIcon}
           style={{ color: "white" }}
-          target="_blank"
-        >
-          <FontAwesomeIcon
-            style={{ color: "white" }}
-            icon={faGithub}
-            fixedWidth
-          />
-        </a>
-      </div>
-      <div className={styles.SocialIcon}>
-<<<<<<< Updated upstream
-        <a
-=======
-        <Link
->>>>>>> Stashed changes
-          href="https://www.instagram.com/monteiro.ops/"
+          icon={faBlog}
+          fixedWidth
+        />
+      </Link>
+      <Link href="/curriculum" style={{ color: "white" }}>
+        <FontAwesomeIcon
+          className={styles.SocialIcon}
           style={{ color: "white" }}
-        >
-          <FontAwesomeIcon
-            style={{ color: "white" }}
-            icon={faInstagram}
-            fixedWidth
-          />
-        </a>
-      </div>
-      <div className={styles.SocialIcon}>
-<<<<<<< Updated upstream
-        <a
-=======
-        <Link
->>>>>>> Stashed changes
-          href="https://br.linkedin.com/in/hernande-monteiro"
+          icon={faUser}
+          fixedWidth
+        />
+      </Link>
+      <Link href="/portfolio" style={{ color: "white" }}>
+        <FontAwesomeIcon
+          className={styles.SocialIcon}
           style={{ color: "white" }}
-        >
-          <FontAwesomeIcon
-            style={{ color: "white" }}
-            icon={faLinkedin}
-            fixedWidth
-          />
-        </a>
-      </div>
+          icon={faNewspaper}
+          fixedWidth
+        />
+      </Link>
+      <Link
+        href="https://github.com/hernandemonteiro"
+        style={{ color: "white" }}
+        target="_blank"
+      >
+        <FontAwesomeIcon
+          className={styles.SocialIcon}
+          style={{ color: "white" }}
+          icon={faGithub}
+          fixedWidth
+        />
+      </Link>
+      <Link
+        href="https://www.instagram.com/monteiro.ops/"
+        style={{ color: "white" }}
+      >
+        <FontAwesomeIcon
+          className={styles.SocialIcon}
+          style={{ color: "white" }}
+          icon={faInstagram}
+          fixedWidth
+        />
+      </Link>
+      <Link
+        href="https://br.linkedin.com/in/hernande-monteiro"
+        style={{ color: "white" }}
+      >
+        <FontAwesomeIcon
+          className={styles.SocialIcon}
+          style={{ color: "white" }}
+          icon={faLinkedin}
+          fixedWidth
+        />
+      </Link>
     </div>
   );
 }
