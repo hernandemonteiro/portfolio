70e12113905961c72a32cfc481bc208c4dab7a62
Hernande Monteiro <monteiro1998@live.com>
2022-11-15 22:16:17 -0300

    hello doc_docs package

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,14 +4,13 @@ import { Analytics } from "@vercel/analytics/react";
 import "../styles/_app.scss";
 import Head from "next/head";
 import Image from "next/image";
-import { initialProps } from "doc_docs/dist/core";
-import ComponentOrDocs from "doc_docs/dist";
+import { initialProps } from "doc_docs/core";
+import ComponentOrDocs from "doc_docs";
 import logo from "../../public/favicon.png";
 
-initialProps(MyApp);
+const configDocs = initialProps();
 
-export default function MyApp(props) {
-
+export default function MyApp({ Component, pageProps, router }) {
   return (
     <>
       <NextNProgress />
@@ -25,20 +24,15 @@ export default function MyApp(props) {
         <link rel="shortcut icon" href="./favicon.png" />
       </Head>
       <ComponentOrDocs
-        route={props.router.route}
-        dirs={props.dirs}
-        subDirs={props.subDirs}
-        Component={props.Component}
-        pageProps={props.pageProps}
-        
+        route={router.route}
+        dirs={configDocs.dirs}
+        subDirs={configDocs.subDirs}
+        Component={Component}
+        pageProps={pageProps}
         projectLogo={
-          <div style={{display: "flex", alignItems: "center"}}>
-            <Image
-              width={50}
-              height={50}
-              src={logo}
-              alt={"logotipo"}
-            /><h3 style={{marginLeft: "2%"}}>Hernande Monteiro</h3>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <Image width={50} height={50} src={logo} alt={"logotipo"} />
+            <h3 style={{ marginLeft: "2%" }}>Hernande Monteiro</h3>
           </div>
         }
         github="https://github.com/hernandemonteiro/personal_blog"
