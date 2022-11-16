bba05f452b16b0b08c9279e12c86d0e52522e056
Hernande Monteiro <monteiro1998@live.com>
2022-11-16 19:28:32 -0300

    modify login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,9 +1,8 @@
-import Head from "next/head";
-import React, { useEffect } from "react";
-import Template from "../../../components/blog/Template";
-import LoginForm from "../../../components/system/LoginForm";
-import LoginPage from "../../../components/system/LoginPage";
-import useAuth from "../../../hooks/useAuthAdmin";
+import React from "react";
+import Template from "../../components/blog/Template";
+import LoginForm from "../../components/system/LoginForm";
+import LoginPage from "../../components/system/LoginPage";
+import useAuth from "../../hooks/useAuthAdmin";
 
 export default function Home() {
   const { setEmail, setPassword, login, redirectLoginPageIfLogged, message } =
