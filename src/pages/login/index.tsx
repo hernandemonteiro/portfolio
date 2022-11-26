f498a280d09472a19f0c91a9c8707b2e4aed428e
Hernande Monteiro <monteiro1998@live.com>
2022-11-26 00:02:09 -0300

    modify import component in login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,9 +1,8 @@
 import React, { useEffect } from "react";
 import Template from "../../components/blog/TemplateBlog";
 import LoginForm from "../../components/system/Login/LoginForm";
-import LoginPage from "../../components/system/LoginPage";
+import LoginPage from "../../components/system/Login/LoginPage";
 import LoaderAdmin from "../../components/ui/Loading";
-import useAuthAdmin from "../../hooks/useAuthAdmin";
 import useAuth from "../../hooks/useAuthAdmin";
 
 export default function Home() {
