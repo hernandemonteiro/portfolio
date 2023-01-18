f48a1e0ed40def587a74aa99de1e87986c910e48
Hernande Monteiro <monteiro1998@live.com>
2023-01-17 21:11:39 -0300

    CI: refactor and test

--- src/pages/article/[id].tsx
+++ src/pages/article/[id].tsx
@@ -6,14 +6,17 @@ import { fetchAPI } from "../../hooks/helpers/fetchAPI";
 
 export async function getServerSideProps(context) {
   const articleID = await context.query.id;
-  const post = await fetchAPI("/api/articles/getByID/" + articleID, "GET");
+  const post = await fetchAPI(`/api/articles/getByID/${articleID}`, "GET");
 
   return { props: { post } };
 }
 
 interface iArtigo {
   post: {
+    _id: string,
     content: string;
+    title: string;
+    resume: string;
   };
 }
 
